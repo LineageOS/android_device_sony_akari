e494c8722344daef09d5c62033cb5d3e551a67cb
Danny Trunk <dtrunk90@gmail.com>
2025-02-16 23:11:04 +0100

    fixup: akari: Adjust override prop key names
    Change-Id: I8913da672df06c45fcc255544d4390195b20e6ce

--- lineage_akari.mk
+++ lineage_akari.mk
@@ -20,6 +20,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-sony-mobile
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="H8296-user 10 52.1.A.3.49 052001A003004902006556692 release-keys" \
-    BuildFingerprint=Sony/H8296/H8296:10/52.1.A.3.49/052001A003004902006556692:user/release-keys \
-    DeviceName=H8296 \
-    DeviceProduct=H8296
+    BuildFingerprint=Sony/H8296/H8296:10/52.1.A.3.49/052001A003004902006556692:user/release-keys
